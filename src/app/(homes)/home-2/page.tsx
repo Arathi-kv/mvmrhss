04f08f744e742e018656cbfd3610d98a3bcaa3ba
Arathi-k-v <arathi@signroots.com>
2025-09-29 14:16:47 +0530

    update

--- src/app/(homes)/home-2/page.tsx
+++ src/app/(homes)/home-2/page.tsx
@@ -6,8 +6,18 @@ import React from 'react';
 import Wrapper from '@/layouts/Wrapper';
 import HeaderTwo from '@/layouts/headers/HeaderTwo';
 import HeroHomeTwo from '@/components/hero/HeroHomeTwo';
-
-
+import AboutHomeTwo from '@/components/about/AboutHomeTwo';
+import ServiceHomeTwo from '@/components/service/ServiceHomeTwo';
+import NewsletterHomeTwo from '@/components/newsletter/NewsletterHomeTwo';
+import ProjectHomeTwo from '@/components/project/ProjectHomeTwo';
+import TestimonialHomeTwo from '@/components/testimonial/TestimonialHomeTwo';
+import TechnologyHomeTwo from '@/components/technology/TechnologyHomeTwo';
+import SolutionHomeTwo from '@/components/solution/SolutionHomeTwo';
+import TeamHomeTwo from '@/components/team/TeamHomeTwo';
+import FaqHomeTwo from '@/components/faq/FaqHomeTwo';
+import CtaHomeTwo from '@/components/cta/CtaHomeTwo';
+import BlogHomeTwo from '@/components/blog/BlogHomeTwo';
+import BrandHomeTwo from '@/components/brand/BrandHomeTwo';
 import FooterOne from '@/layouts/footers/FooterOne';
 
 
@@ -28,7 +38,18 @@ const index = () => {
       <HeaderTwo />
       <main>
         <HeroHomeTwo />
-      
+        <AboutHomeTwo />
+        <ServiceHomeTwo />
+        <NewsletterHomeTwo />
+        <ProjectHomeTwo />
+        <TestimonialHomeTwo />
+        <TechnologyHomeTwo />
+        <SolutionHomeTwo />
+        <TeamHomeTwo />
+        <FaqHomeTwo />
+        <CtaHomeTwo />
+        <BlogHomeTwo />
+        <BrandHomeTwo />
       </main>
       <FooterOne style_2={true} />
     </Wrapper>
